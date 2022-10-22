7583f8afef71bde099a617535ea8283c57f08a28
lautaro avila <lautiavila96@gmail.com>
2022-10-22 18:29:33 -0300

    ISSUE #10[S05]: Se agrega Tipo de Afiliado
    Se agrega a la carga de Afiliado, el tipo enumerado correspondiente.
    * Titular
    * Adherente

--- module-simple/src/main/java/domainapp/modules/simple/types/plan/TipoCobertura.java
+++ module-simple/src/main/java/domainapp/modules/simple/types/plan/TipoCobertura.java
@@ -1,5 +1,23 @@
 package domainapp.modules.simple.types.plan;
 
 public enum TipoCobertura {
-    TOTAL, PARCIAL, PERSONALIZADA
+    PLAN5000("60% en Farmacia", 0),
+    PLAN3000("50% en Farmacia", 850),
+    PLAN100("30% en Farmacia,", 1500);
+
+    private final String farmacia;
+    private final int coseguro;
+
+    TipoCobertura(String farmacia, int coseguro) {
+        this.farmacia = farmacia;
+        this.coseguro = coseguro;
+    }
+
+    public String getFarmacia() {
+        return farmacia;
+    }
+
+    public int getCoseguro() {
+        return coseguro;
+    }
 }
